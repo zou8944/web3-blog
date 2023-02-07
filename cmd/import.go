31558779aa5b37eda12b3e0086cfe97d9133d776
Floyd <894474057@qq.com>
2023-02-07 21:14:57 +0800

    fix: set up all before import articles

--- cmd/import.go
+++ cmd/import.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/project5e/web3-blog/bootstrap"
 	"github.com/project5e/web3-blog/pkg/import/mywords"
 	"github.com/urfave/cli/v2"
 )
 
 func ImportArticles(ctx *cli.Context) error {
+	g := gin.New()
+	bootstrap.SetupAll(g)
 	gitAddress := ctx.Args().First()
 	return mywords.Import(gitAddress)
 }
